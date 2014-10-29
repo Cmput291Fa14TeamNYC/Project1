aca0137d4e50647dc130822aae9ebe8ef9188661
Nancy Pham-Nguyen <nphamngu@ualberta.ca>
2014-10-29 15:32:00 -0600

    Added Menu

--- src/ca/ualberta/cmput291TeamNYC/project1/Main.java
+++ src/ca/ualberta/cmput291TeamNYC/project1/Main.java
@@ -5,11 +5,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Testing ojdbc.jar and connection");
+	/*	System.out.println("Testing ojdbc.jar and connection");
 
 		Connection con = null;
 		Statement stmt= null;
@@ -41,7 +42,44 @@ public class Main {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-		}
+		}*/
+	
+	
+	
+	System.out.println("Main Menu");
+	System.out.println("1.Prescription");
+	System.out.println("2.Medical Test");
+	System.out.println("3.Patient Information Update");
+	System.out.println("4.Search Engine");
+	System.out.println("5.Exit");
+	
+	Scanner input = new Scanner(System.in);
+	int userInput = input.nextInt();
+	
+	switch (userInput) {
+	
+	case 1:
+		System.out.println("1.Prescription");
+		break;
+		
+	case 2:
+		System.out.println("2.Medical Test");
+		break;
+	
+	case 3:
+		System.out.println("3.Patient");
+		break;
+		
+	case 4:
+		System.out.println("4.Search Engine");
+		break;
+		
+	case 5:
+		System.out.println("1.Exit");
+		break;
+	
+	
 	}
 	
+	}
 }
