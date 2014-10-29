df47bd03c1848e198101a9dd7f71c272099d4af5
ayunita <ayunita@hotmail.ca>
2014-10-29 15:54:29 -0600

    Adding attributes for all classes

--- src/ca/ualberta/cmput291TeamNYC/project1/Test_type.java
+++ src/ca/ualberta/cmput291TeamNYC/project1/Test_type.java
@@ -2,4 +2,43 @@ package ca.ualberta.cmput291TeamNYC.project1;
 
 public class Test_type {
 
+	private int type_id;
+	private String test_name;
+	private String pre_requirement;
+	private String test_procedure;
+
+	public Test_type() {
+	}
+
+	public int getType_id() {
+		return type_id;
+	}
+
+	public void setType_id(int type_id) {
+		this.type_id = type_id;
+	}
+
+	public String getTest_name() {
+		return test_name;
+	}
+
+	public void setTest_name(String test_name) {
+		this.test_name = test_name;
+	}
+
+	public String getPre_requirement() {
+		return pre_requirement;
+	}
+
+	public void setPre_requirement(String pre_requirement) {
+		this.pre_requirement = pre_requirement;
+	}
+
+	public String getTest_procedure() {
+		return test_procedure;
+	}
+
+	public void setTest_procedure(String test_procedure) {
+		this.test_procedure = test_procedure;
+	}
 }
