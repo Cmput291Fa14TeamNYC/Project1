150f271522618f02fca8a7b3b6e44274b1a8cfcc
ayunita <ayunita@hotmail.ca>
2014-11-02 09:43:40 -0700

    Adding prescription method v1
    I will reduce the codes as much as possible

--- yunita_src/project1yunita/DataSource.java
+++ yunita_src/project1yunita/DataSource.java
@@ -13,8 +13,8 @@ public class DataSource {
 	private ResultSet rs = null;
 	private final String DRIVERNAME = "oracle.jdbc.driver.OracleDriver";
 	private final String URL = "jdbc:oracle:thin:@localhost:1525:CRS";
-	
-	public DataSource(String username, String password){			
+
+	public DataSource(String username, String password) {
 		try {
 			Class.forName(DRIVERNAME);
 			con = DriverManager.getConnection(URL, username, password);
@@ -27,16 +27,138 @@ public class DataSource {
 			e.printStackTrace();
 		}
 	}
-	
-	public void select(String column, String tablename){
+
+	/*
+	 * The program shall allow a user to enter the detailed information about
+	 * the prescription: >the employee_no or name of the doctor who prescribes
+	 * the test >the test name >the name and/or the health_care_no of the
+	 * patient
+	 */
+
+	public void enterPrescription(int employee_no, String test_name,
+			String patient_no) {
+		
+		// check test name, return test id
+		// check patient id, return name
+
+		int doctor_id = 0;
+		int patient_id = 0;
+		int type_id = 0;
+		int not_allowed = 0;
+
 		try {
-			rs = stmt.executeQuery("SELECT " + column + " FROM " + tablename);
-			while (rs.next()) {
-				System.out.println(rs.getString("name"));
+			// check whether the doctor exists in doctor table
+			if (selectDoctorByEmpNo(employee_no).next()) { // return a single row
+				System.out.println("Doctor name: " + rs.getString("name"));
+				doctor_id = rs.getInt("employee_no");
+			}
+			// check whether the patient exists in patient table
+			if (selectPatient(patient_no).next()) {
+				System.out.println("Patient name: " + rs.getString("name"));
+				patient_id = rs.getInt("health_care_no");
+			}
+			// check whether the test type exists in test type table
+			if (selectTestByName(test_name).next()) {
+				System.out.println("Test name: " + rs.getString("type_id"));
+				type_id = rs.getInt("type_id");
+			}
+			if (selectNotAllowedByPatient(patient_id).next()) {
+				System.out.println("Not allowed: " + rs.getString("test_id"));
+				not_allowed = rs.getInt("test_id");
+			}
+
+			// check whether the patient is allowed to take the test type
+			if (type_id == not_allowed) {
+				System.out
+						.println("Sorry this patient is not allowed to take this test!");
+			} else {
+				// inserting into record
+				String insertRecordQuery = "INSERT INTO test_record VALUES ("
+						+ getLastId("test_id", "test_record") + "," + type_id
+						+ "," + patient_id + "," + doctor_id + "," + "null"
+						+ "," + "null" + "," + "sysdate" + "," + "null" + ")";
+				stmt.execute(insertRecordQuery);
+				con.commit();
+				System.out.println("New record is inserted.");
 			}
 		} catch (SQLException e) {
-			System.out.println("Patient table does not exist.");
-			e.printStackTrace();
+			System.out.println("Cannot insert values into Record table.");
 		}
 	}
+
+	public static boolean isInteger(String s) {
+		try {
+			Integer.parseInt(s);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public ResultSet selectPatient(String patient_info) {
+		try {
+			// checking whether the user enters health care no or patient name
+			if (isInteger(patient_info)) {
+				String checkPatientByNoQuery = "SELECT * FROM PATIENT WHERE health_care_no = "
+						+ Integer.parseInt(patient_info);
+				rs = stmt.executeQuery(checkPatientByNoQuery);
+			} else {
+				String checkPatientByNameQuery = "SELECT * FROM PATIENT WHERE name = '"
+						+ patient_info + "'";
+				rs = stmt.executeQuery(checkPatientByNameQuery);
+			}
+		} catch (SQLException e) {
+			System.out.println("Cannot execute select patient statement.");
+		}
+		return rs;
+	}
+
+	public ResultSet selectDoctorByEmpNo(int employee_no) {
+		String checkDoctorQuery = "SELECT * FROM PATIENT p, DOCTOR d "
+				+ "WHERE d.health_care_no = p.health_care_no AND d.employee_no = "
+				+ employee_no;
+		try {
+			rs = stmt.executeQuery(checkDoctorQuery);
+		} catch (SQLException e) {
+			System.out.println("Cannot execute select doctor statement.");
+		}
+		return rs;
+	}
+
+	public ResultSet selectTestByName(String test_name) {
+		String checkTestQuery = "SELECT * FROM TEST_TYPE WHERE test_name = '"
+				+ test_name + "'";
+		try {
+			rs = stmt.executeQuery(checkTestQuery);
+		} catch (SQLException e) {
+			System.out.println("Cannot execute select test type statement.");
+		}
+		return rs;
+	}
+
+	public ResultSet selectNotAllowedByPatient(int patient_no) {
+		String checkNotAllowedQuery = "SELECT * FROM NOT_ALLOWED WHERE health_care_no = "
+				+ patient_no;
+		try {
+			rs = stmt.executeQuery(checkNotAllowedQuery);
+		} catch (SQLException e) {
+			System.out.println("Cannot execute select not allowed statement.");
+		}
+		return rs;
+	}
+
+	public int getLastId(String col, String table) {
+		String lastIdQuery = "SELECT MAX(" + col + ") AS ID FROM " + table;
+		int lastId = 0;
+		try {
+			rs = stmt.executeQuery(lastIdQuery);
+			if (rs.next()) {
+				lastId = rs.getInt("id");
+			}
+		} catch (SQLException e) {
+			System.out.println("Cannot get the last id from " + table);
+		}
+		return lastId+1;
+	}
+
 }
--- yunita_src/project1yunita/Main.java
+++ yunita_src/project1yunita/Main.java
@@ -5,14 +5,77 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
 
+	public Main(){
+		DataSource ds = new DataSource("commande", "Alexsq2014");
+		menu(ds);
+
+	}
+	
+	public void menu(DataSource ds){
+		System.out.println("Main Menu");
+
+		System.out.println("1.Prescription");
+		System.out.println("2.Medical Test");
+		System.out.println("3.Patient Information Update");
+		System.out.println("4.Search Engine");
+		System.out.println("5.Exit");
+		
+		System.out.print("Option: ");
+		Scanner input = new Scanner(System.in);
+		int userInput = input.nextInt();
+		
+		switch (userInput) {
+		
+		case 1:
+			System.out.println("1.Prescription");
+			prescription(ds);
+			break;
+			
+		case 2:
+			System.out.println("2.Medical Test");
+			break;
+		
+		case 3:
+			System.out.println("3.Patient");
+			break;
+			
+		case 4:
+			System.out.println("4.Search Engine");
+			break;
+			
+		case 5:
+			System.out.println("5.Exit");
+			break;
+		}
+	}
+	
+	public void prescription(DataSource ds){
+		// enter employee no
+		System.out.print("Enter employee no: ");
+		Scanner input1 = new Scanner(System.in);
+		int employee_no = input1.nextInt();
+		
+		// enter health care no or patient name
+		System.out.print("Enter health care no or patient name: ");
+		Scanner input2 = new Scanner(System.in);
+		String patient_info = input2.nextLine();
+		
+		// enter test name
+		System.out.print("Enter test name: ");
+		Scanner input3 = new Scanner(System.in);
+		String test_name = input3.nextLine();
+		System.out.println(test_name);
+		
+		ds.enterPrescription(employee_no, test_name, patient_info);
+		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Yunita");
-	
-		DataSource ds = new DataSource("commande", "Alexsq2014");
-		ds.select("*", "PATIENT");
-		
+		Main m = new Main();
 	}
 }
